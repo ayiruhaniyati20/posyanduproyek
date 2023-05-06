65dc4a97e580d1238f31b54b95bacad0bc7d4a2e
Your Name <you@example.com>
2023-05-07 02:10:12 +0700

    commit pertama

---
+++ resources/views/tambahdata.blade.php
@@ -0,0 +1,62 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <title>CRUD laravel 8</title>
+  </head>
+
+  <body>
+    <h1 class="text-center mb-4">Tambah Data Pegawai</h1>
+        
+    <div class="container">
+      
+      <div class="row justify-content-center">
+         <div class="col-8">
+          <div class="card">
+            <div class="card-body">
+             <form action="/insertdata" method="POST" enctype="multipart/form-data">
+              @csrf
+               <div class="mb-3">
+                 <label for="exampleInputEmail1" class="form-label">Nama Lengkap</label>
+                 <input type="text" name="nama" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+               </div>
+               <div class="mb-3">
+                <label for="exampleInputEmail1" class="form-label">Jenis Kelamin</label>
+               <select class="form-select" name="jeniskelamin" aria-label="Default select example">
+                      <option selected>Pilih Jenis Kelamin</option>
+                      <option value="cowo">cowo</option>
+                      <option value="cewe">cewe</option>
+                    </select>
+              </div>
+              <div class="mb-3">
+                <label for="exampleInputEmail1" class="form-label">Nomer Telepon</label>
+                <input type="number" name="nomertelepon" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+              </div>
+
+              <div class="mb-3">
+                <label for="exampleInputEmail1" class="form-label">Masukan Foto</label>
+                <input type="file" name="foto" class="form-control" >
+              </div>
+
+
+
+
+
+
+
+
+
+               <button type="submit" class="btn btn-primary">Submit</button>
+             </form>
+            </div>
+           </div>
+         </div>
+
+      </div>
+    </div>
